4ecf1cfd19858361de17ce5effdc64b8a14bc350
Ian VanSchooten <ian.vanschooten@gmail.com>
2016-12-30 09:10:16 -0500

    Fix some indenting

--- util/displayUtil.js
+++ util/displayUtil.js
@@ -45,11 +45,11 @@ function getObjectOutput(stats) {
 
   return _.map(sortedRuleNames, function (ruleId) {
     return _.map(stats[ruleId], function (count, severity) {
-        return _.padRight(ruleId + ': ', maxRuleLength + 2) +
-          chalk.magenta(_.padLeft(count, maxResultLength)) + '|' +
-          getBar(normalizeBarLength(count, maxResult, maxBarLength), barColors[severity]);
-      }).join('\n');
-    }, '').join('\n') + '\n';
+      return _.padRight(ruleId + ': ', maxRuleLength + 2) +
+        chalk.magenta(_.padLeft(count, maxResultLength)) + '|' +
+        getBar(normalizeBarLength(count, maxResult, maxBarLength), barColors[severity]);
+    }).join('\n');
+  }, '').join('\n') + '\n';
 }
 
 function isAnyRuleStacked(stats) {
